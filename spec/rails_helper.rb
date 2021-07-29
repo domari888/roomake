f029c1ba06c445d88f0c441e022ab018bc05b44e
damari <motokin128@gmail.com>
2021-07-29 15:32:30 +0900

    Faker の環境構築

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+# 日本語のダミーデータを使用
+Faker::Config.locale = :ja
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
