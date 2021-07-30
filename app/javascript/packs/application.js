9ef9d21c308e5b9b20c2da94fc303c5a74680ba4
damari <motokin128@gmail.com>
2021-07-30 12:18:26 +0900

    Bootstrap をインストール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// Bootstrap をインクルード
+import "bootstrap/dist/js/bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
