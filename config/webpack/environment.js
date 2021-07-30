25f72e2e6d5a91842208d50d1373f8ffb2a28df2
damari <motokin128@gmail.com>
2021-07-30 12:19:29 +0900

    jQuery の設定を追加
    Bootstrap 以外でも jQuery を使用可能にするため

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
-
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugins({
+  $: 'jquery',
+  jQuery: 'jquery'
+}))
 module.exports = environment
