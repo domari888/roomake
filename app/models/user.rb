9075d2f985456d08e848a7cf3c10d93fac1d1cce
damari <motokin128@gmail.com>
2021-08-01 15:58:03 +0900

    カラムを追加して バリデーション、制約を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :age, presence: true
+  validates :address, presence: true
+  validates :profile, length: { maximum: 2000 }
+  validates :household, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_083327) do
+ActiveRecord::Schema.define(version: 2021_08_01_061841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_07_31_083327) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.integer "age", default: 0, null: false
+    t.integer "address", default: 0, null: false
+    t.text "profile"
+    t.string "avater"
+    t.string "favorite_items"
+    t.integer "household", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
