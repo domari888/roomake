1772f60219ac04825ab4e4c7d1fdc8a6fbacd077
damari <motokin128@gmail.com>
2021-08-09 19:27:18 +0900

    User モデルのモデルスペックを実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-# 日本語のダミーデータを使用
-Faker::Config.locale = :ja
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+# 日本語のダミーデータを使用
+Faker::Config.locale = :ja
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
