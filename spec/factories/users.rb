eda7a2351b9e623f032ecda5888f9e027b2a584f
damari <motokin128@gmail.com>
2021-08-09 12:33:56 +0900

    factory_bot_rails を使った初期データの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 8) }
+    age { rand(1..7) }
+    address { rand(1..47) }
+    household { rand(1..6) }
+    profile { Faker::Lorem.sentence }
+    avater { File.open("#{Rails.root}/public/images/fallback/default.png") }
   end
 end
