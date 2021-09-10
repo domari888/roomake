082ccdd41cae3ae0977e38c362899109aed78ed3
damari <motokin128@gmail.com>
2021-09-10 12:12:38 +0900

    fix: 変数の定義方法を変更

--- app/javascript/packs/post_preview.js
+++ app/javascript/packs/post_preview.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function () {
   $(function(){
     //DataTransferオブジェクトで、データを格納する箱を作る
-    var dataBox = new DataTransfer();
+    let dataBox = new DataTransfer();
     //querySelectorでfile_fieldを取得
-    var file_field = document.querySelector('input[type=file]')
+    let file_field = document.querySelector('input[type=file]')
     //file_field の値が変更されたときに発火するイベント
     $('#post-preview-image').on("change", function(){
 
@@ -15,9 +15,9 @@ $(document).on('turbolinks:load', function () {
       }
 
       // 選択したfileのオブジェクトをpropで取得
-      var files = $('input[type="file"]').prop('files')[0];
+      let files = $('input[type="file"]').prop('files')[0];
       $.each(this.files, function(i, file){
-        var fileReader = new FileReader();
+        let fileReader = new FileReader();
         
         // DataTransferオブジェクトに対して、fileを追加
         dataBox.items.add(file)
@@ -27,7 +27,7 @@ $(document).on('turbolinks:load', function () {
         fileReader.readAsDataURL(file);
         // 読み込んだ URL を src に格納して プレビューの HTML を作成
         fileReader.onloadend = function() {
-          var html = `<div class="preview-item" data-image="${file.name}">
+          let html = `<div class="preview-item" data-image="${file.name}">
                         <img src="${fileReader.result}" class="preview-image">
                         <button type="button" class="btn btn-dark btn-sm delete-preview rounded-circle"><i class="fas fa-times"></i></button>
                       </div>`
