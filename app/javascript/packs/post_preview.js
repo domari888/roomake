04f2c81a3e157f0f780c53694354e34eabbe51f3
damari <motokin128@gmail.com>
2021-09-11 13:47:17 +0900

    add: 投稿画像の枚数を最大で６枚に制限

--- app/javascript/packs/post_preview.js
+++ app/javascript/packs/post_preview.js
@@ -23,30 +23,27 @@ $(document).on('turbolinks:load', function () {
         dataBox.items.add(file)
         // DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
         file_field.files = dataBox.files
-        
+
         fileReader.readAsDataURL(file);
+
+        // 画像が6枚以上になった場合、input を非表示
+        let preview_item_length = $('.preview-item').length + i +1;
+        if ( preview_item_length >= 6 ){
+          $('#post-preview-image').hide();
+          if ( preview_item_length == 7 ){
+            $('#new-button').prop('disabled', true);
+            alert('画像は最大 6枚 にしてください');
+          } 
+        }
+
         // 読み込んだ URL を src に格納して プレビューの HTML を作成
         fileReader.onloadend = function() {
           let html = `<div class="preview-item" data-image="${file.name}">
                         <img src="${fileReader.result}" class="preview-image">
-                        <button type="button" class="btn btn-dark btn-sm delete-preview rounded-circle"><i class="fas fa-times"></i></button>
                       </div>`
           $('#preview-box').append($(html));
         };
       });
-
-      // プレビューの削除ボタンをクリックしたとき、発火するイベント
-      $(document).on("click", '.delete-preview', function(){
-        let target_preview = $(this).parents('.preview-item')
-        let target_image = $(target_preview).data('image')
-        $.each(file_field.files, function(i, file){
-          if( file.name === target_image ){
-            dataBox.items.remove(i)
-          }
-        });
-        target_preview.remove();
-        file_field.files = dataBox.files
-      })
     });
   });
 });
