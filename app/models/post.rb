18208d7b8bde2b449e9e5f4a204e55c96ae1d3c5
damari <motokin128@gmail.com>
2021-09-22 02:34:54 +0900

    add: user モデルと post モデルにアソシエーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   # user モデルの name を委譲する
   delegate :name, :avater, to: :user, prefix: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :comments
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :age, presence: true
