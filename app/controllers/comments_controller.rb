1868257807fc437e318062bb176065f7c562fd28
damari <motokin128@gmail.com>
2021-09-22 15:14:10 +0900

    add: comments コントローラを作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.save!
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy!
+  end
+
+  private
+
+  def set_post
+    @post = Post.find[params[:post_id]]
+  end
+
+  def comment_params
+    params.required(:comment).permit(:content, :user_id, :post_id)
+  end
+end
