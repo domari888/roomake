3cef5ffc19785c743bb39f33d88e3679a26657d2
damari <motokin128@gmail.com>
2021-09-22 11:53:21 +0900

    add: user モデルに dependent: :destroy のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :age, presence: true
