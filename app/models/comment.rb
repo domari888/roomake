293a0cdb7d6e29c616a53ab8b2959086adc8195a
damari <motokin128@gmail.com>
2021-09-26 23:48:50 +0900

    update: デメテルの法則に従い user モデルを委譲

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :content, presence: true, length: { maximum: 140 }
+
+  # user  モデルの name と avater を委譲する
+  delegate :name, :avater, to: :user, prefix: true
 end
