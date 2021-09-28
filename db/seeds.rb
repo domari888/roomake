ffcfd417bc18f69378a13ea829efe4f8e469bd52
damari <motokin128@gmail.com>
2021-09-28 23:17:59 +0900

    add: タグの初期データを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ address = 'kanagawa'
 household = 'two_person_household'
 image = File.open(Rails.root.join('public/images/fallback/test.png'))
 content = 'コメント内容を表示します'
+tags = ['キッチン', 'リビング', 'バス', 'トイレ', 'バルコニー', '洗面台', '寝室', '洋室', '和室', '子供部屋', '玄関', 'その他']
 
 User.find_or_create_by!(email: email) do |user|
   user.name = name
@@ -24,6 +25,12 @@ User.find_or_create_by!(email: email) do |user|
   user.household = household
   puts 'ユーザーの初期データインポートに成功しました。'
 end
+
+ActiveRecord::Base.connection.execute('TRUNCATE TABLE tags RESTART IDENTITY CASCADE')
+tags.each do |tag|
+  Tag.create!(name: tag)
+end
+puts 'タグの初期データインポートに成功しました。'
 
 # テスト投稿の初期データ
 ActiveRecord::Base.connection.execute('TRUNCATE TABLE posts RESTART IDENTITY CASCADE')
@@ -43,3 +50,9 @@ post1.comments.create!(content: content, user_id: 1)
 post2.comments.create!(content: content, user_id: 1)
 post3.comments.create!(content: content, user_id: 1)
 puts 'コメントの初期データインポートに成功しました。'
+
+ActiveRecord::Base.connection.execute('TRUNCATE TABLE post_tags RESTART IDENTITY CASCADE')
+post1.post_tags.create!(tag_id: 1)
+post2.post_tags.create!(tag_id: 2)
+post3.post_tags.create!(tag_id: 3)
+puts '投稿タグの初期データインポートに成功しました。'
