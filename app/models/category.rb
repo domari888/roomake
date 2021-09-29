3bc23ca5b7943b53d566e96a526206431cf27211
damari <motokin128@gmail.com>
2021-09-30 03:00:07 +0900

    add: PostCategory モデルを作成して、Post モデルと Category モデルの関連付け・制約・バリデーションを設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
-  validates :categories, presence: true
+  has_many :post_categories, dependent: :destroy
+  has_many :posts, through: :post_categories
+  validates :name, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+  has_many :post_categories, dependent: :destroy
+  has_many :categories, through: :post_categories
 
   # user モデルの name を委譲する
   delegate :name, :avater, to: :user, prefix: true
