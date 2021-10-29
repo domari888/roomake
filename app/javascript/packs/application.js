1cedee54836edc5f4abb69c31ffeb82b8eeeafb7
damari <motokin128@gmail.com>
2021-10-29 13:52:10 +0900

    add: photos_preview.js ファイルを作成して、画像プレビュー機能の処理を共通化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ import "bootstrap/dist/js/bootstrap"
 // FontAwesome をインクルード
 import "@fortawesome/fontawesome-free/js/all"
 import "./preview"
-import "./post_preview"
+import "./photos_preview"
 import "./post_edit"
 
 Rails.start()
