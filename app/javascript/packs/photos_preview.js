3c2675fb052b86386b5377ed2bd6faff4c2f7ef1
damari <motokin128@gmail.com>
2021-10-30 15:08:46 +0900

    clean: js ファイルの変数宣言方法を let から const に変更,及び余分なものを削除

--- app/javascript/packs/photos_preview.js
+++ app/javascript/packs/photos_preview.js
@@ -7,7 +7,7 @@ $(document).on('turbolinks:load', function () {
     // 画像選択時にプレビューを表示
     $('.post-preview-image').on("change", function(){
       let dataBox, fileField
-      let action = $(this).data('action');
+      const action = $(this).data('action');
       if (action === "edit") {
         dataBox = editDataBox
         fileField = edit_file_field
@@ -15,21 +15,21 @@ $(document).on('turbolinks:load', function () {
         dataBox = newDataBox
         fileField = new_file_field
       }
-      let files = $('input[type="file"]').prop('files')[0];
+      const files = $('input[type="file"]').prop('files')[0];
       $.each(this.files, function(i, file){
-        let fileReader = new FileReader();
-        let lastFileId = dataBox.files.length === 0 ? 0 : $(dataBox.files).last()[0].id;
+        const fileReader = new FileReader();
+        const lastFileId = dataBox.files.length === 0 ? 0 : $(dataBox.files).last()[0].id;
         file.id = lastFileId + 1;
         dataBox.items.add(file);
         fileField.files = dataBox.files
         fileReader.readAsDataURL(file);
         fileReader.onloadend = function() {
-          let html = `<div class="preview-item" data-id="${file.id}">
+          const html = `<div class="preview-item" data-id="${file.id}">
                         <img src="${fileReader.result}" class="preview-image">
                         <button type="button" class="btn btn-dark btn-sm rounded-circle delete-preview" data-action="${action}"><i class="fas fa-times"></i></button>
                       </div>`;
           $(`#${action}-drop`).before($(html));
-          let previewItemLength = $(`#${action}-drop`).prevAll('.preview-item').length;
+          const previewItemLength = $(`#${action}-drop`).prevAll('.preview-item').length;
           if ( previewItemLength >= 6 ){
             $(`#${action}-drop`).hide();
             if ( previewItemLength == 7 ){
@@ -43,7 +43,7 @@ $(document).on('turbolinks:load', function () {
     // 画像の削除
     $('.preview-box').on("click", '.delete-preview', function(){
       let dataBox, fileField;
-      let action = $(this).data('action');
+      const action = $(this).data('action');
       if (action === "edit") {
         dataBox = editDataBox
         fileField = edit_file_field
@@ -51,8 +51,8 @@ $(document).on('turbolinks:load', function () {
         dataBox = newDataBox
         fileField = new_file_field
       }
-      let targetImage = $(this).parents('.preview-item');
-      let targetId = $(targetImage).data('id');
+      const targetImage = $(this).parents('.preview-item');
+      const targetId = $(targetImage).data('id');
       $.each(fileField.files, function(i, file){
         if(file.id === targetId){
           dataBox.items.remove(i);
@@ -66,7 +66,7 @@ $(document).on('turbolinks:load', function () {
         $('#edit-drop').before(`<input type="hidden" name="post_form[delete_ids][]" value="${deleteId}">`);
       }
       targetImage.remove();
-      let previewItemLength = $(`#${action}-drop`).prevAll('.preview-item').length;
+      const previewItemLength = $(`#${action}-drop`).prevAll('.preview-item').length;
       if ( previewItemLength <= 6 ){
         $(`#${action}-button`).prop('disabled', false);
         if ( previewItemLength <= 5 ){
--- app/javascript/packs/post_edit.js
+++ app/javascript/packs/post_edit.js
@@ -1,29 +1,29 @@
 $(document).on('turbolinks:load', function() {
   $(function() {
-    $('#edit-modal').on('show.bs.modal', function (event) {
-      let content = gon.edit_content
-      let photos = gon.edit_photos
-      let tags = gon.edit_tags
-      let categories = gon.edit_categories
+    $('#edit-modal').on('show.bs.modal', function () {
+      const content = gon.edit_content
+      const photos = gon.edit_photos
+      const tags = gon.edit_tags
+      const categories = gon.edit_categories
       // 投稿内容を表示
       $('#content-form').val(content)
       // 投稿画像を表示
-      let previewData = $('#edit-drop').prevAll('.preview-item');
-      let targetIds = $.map(previewData, function(preview) {
+      const previewData = $('#edit-drop').prevAll('.preview-item');
+      const targetIds = $.map(previewData, function(preview) {
         return $(preview).data('id');
       });
-      let photoIds = $.map(photos, function(photo) {
+      const photoIds = $.map(photos, function(photo) {
         return photo.id;
       });
       if (targetIds.toString() !== photoIds.toString()) {
         $('#edit-drop').prevAll().remove();
         photos.forEach(function(photo){
-        let html = `<div class="preview-item" data-id="photos-${photo.id}">
+        const html = `<div class="preview-item" data-id="photos-${photo.id}">
                         <img src="${photo.image.url}" class="preview-image">
                         <button type="button" class="btn btn-dark btn-sm rounded-circle delete-preview" id="photo-delete" data-action="edit"><i class="fas fa-times"></i></button>
                       </div>`;
           $('#edit-drop').before($(html));
-          let previewItemLength = $('#edit-drop').prevAll('.preview-item').length;
+          const previewItemLength = $('#edit-drop').prevAll('.preview-item').length;
           if ( previewItemLength >= 6 ){
             $('#edit-drop').hide();
           }
