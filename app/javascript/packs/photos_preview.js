6d1653e5802d4c73bd34a180e6c4c12f74100cc4
damari <motokin128@gmail.com>
2021-10-30 14:10:25 +0900

    add: 新規投稿・編集で共通化したドラッグ＆ドロップの機能を作成

--- app/javascript/packs/photos_preview.js
+++ app/javascript/packs/photos_preview.js
@@ -74,5 +74,63 @@ $(document).on('turbolinks:load', function () {
         }
       }
     });
+
+    $('.modal').on('show.bs.modal', function (e) {
+      let dropArea;
+      const modalAction = this.id
+      if (modalAction === "edit-modal") {
+        dropArea = document.getElementById('edit-drop');
+      } else {
+        dropArea = document.getElementById('new-drop');
+      }
+      //ドロップエリアの上にある時に発火するイベント
+      dropArea.addEventListener("dragover", function(e){
+        e.preventDefault();
+        $(this).css({'border': '2px solid #9e9e9e', 'opacity': '0.5'});
+      },false);
+      //ドロップエリアから離れた時に発火するイベント
+      dropArea.addEventListener("dragleave", function(e){
+        e.preventDefault();
+        $(this).css({'border': '2px dashed #9c9c9c', 'background': '#f7f7f7', 'opacity': '1.0'});
+      },false);
+      // ドロップしたときに発火するイベント
+      dropArea.addEventListener("drop", function(e) {
+        let dataBox, fileField;
+        const action = $(this).children().data('action');
+        if (action === "edit") {
+          dataBox = editDataBox
+          fileField = edit_file_field
+        } else {
+          dataBox = newDataBox
+          fileField = new_file_field
+        }  
+        e.preventDefault();
+        $(this).css({'border': '2px dashed #9c9c9c', 'background': '#f7f7f7', 'opacity': '1.0'});
+        const files = e.dataTransfer.files;
+        $.each(files, function(i, file){
+          const fileReader = new FileReader();
+          const lastFileId = dataBox.files.length === 0 ? 0 : $(dataBox.files).last()[0].id;
+          file.id = lastFileId + 1;
+          dataBox.items.add(file)
+          fileField.files = dataBox.files
+          fileReader.readAsDataURL(file);
+          fileReader.onloadend = function() {
+            const html = `<div class="preview-item" data-id="${file.id}">
+                        <img src="${fileReader.result}" class="preview-image">
+                        <button type="button" class="btn btn-dark btn-sm rounded-circle delete-preview" data-action="${action}"><i class="fas fa-times"></i></button>
+                      </div>`;
+            $(`#${action}-drop`).before($(html));
+            const previewItemLength = $(`#${action}-drop`).prevAll('.preview-item').length;
+            if ( previewItemLength >= 6 ){
+              $(`#${action}-drop`).hide();
+              if ( previewItemLength == 7 ){
+                $(`#${action}-button`).prop('disabled', true);
+                alert('画像は最大 6枚 にしてください');
+              }
+            }
+          };
+        });
+      });
+    });
   });
 });
