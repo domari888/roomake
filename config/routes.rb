9e17849fe459ced0eee5f683f052e111a3a8a1a1
damari <motokin128@gmail.com>
2021-11-01 18:53:55 +0900

    add: users コントローラを作成して、ルーティングを設定
    ユーザーのマイページを作成するため

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#index'
+  resources :users, only: %i[show]
   resources :posts, except: %i[new edit] do
     resources :comments, only: %i[create destroy]
   end
