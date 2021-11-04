91303ed11bb8eb9ee6a755fb97e2fae8d18e4adb
damari <motokin128@gmail.com>
2021-11-04 21:05:58 +0900

    add: kaminari:views bootstrap4 のビューファイルを作成して、ページネーションを表示
    ページネーションのスタイルを整えるため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  PER_PAGE = 9
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = Post.includes(:photos).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
   end
 end
