7c37012066484dd1052236810b36305b813b7887
damari <motokin128@gmail.com>
2021-11-08 15:03:55 +0900

    add: container のサイズが sm 以下の場合に、ユーザーページの投稿表示を無限スクロールで表示するように実装
    画面縦長での表示の場合、スクロール表示の方が UI が向上するため

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ import "./post_edit"
 import "./jquery.validate"
 import "./jquery.validate.min"
 import "./validate_rules"
+import "./user_show"
 
 Rails.start()
 Turbolinks.start()
---
+++ app/javascript/packs/user_show.js
@@ -0,0 +1,20 @@
+$(document).on('turbolinks:load', function(){
+  $(function(){
+    const windowSize = $(window).width();
+    if (windowSize < 576) {
+      const jscrollOption = {
+        loadingHtml: '<div class="d-flex justify-content-center"><div class="spinner-border text-secondary" role="status"><span class="sr-only">Loading...</span></div></div>',
+        autoTrigger: true,
+        padding: 100,
+        nextSelector: 'a[rel=next]',
+        contentSelector: '.jscroll',
+        pagingSelector: '.pagination',
+        callback: function(){
+          console.log('ok')
+          $('.pagination').hide();
+        }
+      };
+      $('.jscroll').jscroll(jscrollOption);
+    }
+  });
+});
