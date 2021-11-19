239fb0f0682a2a842ce7225a0204e30c6cf445e1
damari <motokin128@gmail.com>
2021-11-19 21:40:27 +0900

    add: いいね機能を実装
    js.erb を使用して非同期でいいね表示を切り替えられるように設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
+  def create
+    current_user.likes.create!(post_id: @post.id)
+  end
+
+  def destroy
+    current_user.likes.find_by(post_id: @post.id).destroy!
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,8 @@ class Post < ApplicationRecord
 
   # user モデルの name を委譲する
   delegate :name, :avater, :id, to: :user, prefix: true
+
+  def liked_by?(current_user)
+    likes.exists?(user_id: current_user.id)
+  end
 end
