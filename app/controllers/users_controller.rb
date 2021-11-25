47540066b0a987782c80bdfabdd96c745ff762b3
damari <motokin128@gmail.com>
2021-11-26 03:32:24 +0900

    add: ユーザーページのタブに「いいね」した投稿の一覧を表示
    ページネーションを使い分けるために、param_name を設定してページ番号を取得。部分テンプレートをユーザーの投稿・いいねした投稿で共通化するために パーシャルとコレクションを設定して、 ページネーションは Ajax で 9 件ごとに表示。画面サイズが sm 以下の場合のみ jscroll での無限スクロールで表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   PER_PAGE = 9
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.includes(:photos).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+    @posts = @user.posts.includes(:photos).order(created_at: :desc).page(params[:post_page]).per(PER_PAGE)
+    @likes = @user.liked_posts.includes(:photos).order(created_at: :desc).page(params[:like_page]).per(PER_PAGE)
     @page = params[:page]
   end
 end
