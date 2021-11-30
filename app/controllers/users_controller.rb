c9fb1311cb4c0d17df667d4209e18a7d6591a3d0
damari <motokin128@gmail.com>
2021-11-30 17:17:08 +0900

    add: ユーザーページのタブに「マーク」した投稿の一覧を表示
    ページネーションを使い分けるために param_name を使用してページ番号を取得して Ajax で実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:photos).order(created_at: :desc).page(params[:post_page]).per(PER_PAGE)
     @likes = @user.liked_posts.includes(:photos).order(created_at: :desc).page(params[:like_page]).per(PER_PAGE)
+    @marks = @user.marked_posts.includes(:photos).order(created_at: :desc).page(params[:mark_page]).per(PER_PAGE)
     @page = params[:page]
   end
 end
