c86054daa9c8b68e864d70cd4e281d22010f080b
damari <motokin128@gmail.com>
2021-12-01 22:26:22 +0900

    add: ユーザーページのタブコンテンツを jscroll で実装
    タブ切り替え時に data 属性を取得して各コンテンツごとに切り替え一覧を表示できるように  jscroll.js ファイルの処理を共通化

--- app/javascript/packs/jscroll.js
+++ app/javascript/packs/jscroll.js
@@ -2,18 +2,31 @@ $(document).on('turbolinks:load', function(){
   $(function(){
     const windowSize = $(window).width();
     if (windowSize < 576) {
-      const jscrollOption = {
-        loadingHtml: '<div class="d-flex justify-content-center"><div class="spinner-border text-secondary" role="status"><span class="sr-only">Loading...</span></div></div>',
-        autoTrigger: true,
-        padding: 100,
-        nextSelector: 'a[rel=next]',
-        contentSelector: '.jscroll',
-        pagingSelector: '.pagination',
-        callback: function(){
-          $('.pagination').hide();
-        }
-      };
-      $('.jscroll').jscroll(jscrollOption);
+      function jscrollOption(tabId){
+        const Option = {
+          loadingHtml: '<div class="d-flex justify-content-center"><div class="spinner-border text-secondary" role="status"><span class="sr-only">Loading...</span></div></div>',
+          autoTrigger: true,
+          padding: 100,
+          nextSelector: 'a[rel=next]',
+          contentSelector: `#${tabId}`,
+          pagingSelector: '.pagination',
+          callback: function(){
+            $(this).children().addClass('show');
+            $('.pagination').hide();
+          }
+        };
+        $(`#${tabId}`).jscroll(Option);
+      }
+
+      // ユーザーページアクセス時にアクティブ状態のタブに jscroll を設定
+      const activeId = $('.tab-pane.active')[0].id;
+      jscrollOption(activeId);
+
+      // タブ切り替え表示の際にアクティブ状態のタブに jscroll を設定
+      $('a[data-toggle="list"]').on('shown.bs.tab', function (e) {
+        const tabId = e.target.dataset.tab;
+        jscrollOption(tabId);
+      });
     }
   });
 });
