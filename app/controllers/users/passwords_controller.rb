ba431f733cdf6cc14db32e7f08d8edf5a369ce0b
damari <motokin128@gmail.com>
2021-12-05 15:01:43 +0900

    clean: rubocop -A を実行
    リファクタリングのため

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,40 +1,42 @@
 # frozen_string_literal: true
 
-class Users::PasswordsController < Devise::PasswordsController
-  before_action :ensure_nomal_user, only: :create
+module Users
+  class PasswordsController < Devise::PasswordsController
+    before_action :ensure_nomal_user, only: :create
 
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
+    # GET /resource/password/new
+    # def new
+    #   super
+    # end
 
-  # POST /resource/password
-  # def create
-  #   super
-  # end
+    # POST /resource/password
+    # def create
+    #   super
+    # end
 
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
+    # GET /resource/password/edit?reset_password_token=abcdef
+    # def edit
+    #   super
+    # end
 
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
+    # PUT /resource/password
+    # def update
+    #   super
+    # end
 
-  # protected
+    # protected
 
-  def ensure_nomal_user
-    redirect_to new_user_session_path, alert: 'ゲストユーザーのパスワード再設定はできません' if params[:user][:email].downcase == 'guest@example.com'
+    def ensure_nomal_user
+      redirect_to new_user_session_path, alert: 'ゲストユーザーのパスワード再設定はできません' if params[:user][:email].casecmp('guest@example.com').zero?
+    end
+
+    # def after_resetting_password_path_for(resource)
+    #   super(resource)
+    # end
+
+    # The path used after sending reset password instructions
+    # def after_sending_reset_password_instructions_path_for(resource_name)
+    #   super(resource_name)
+    # end
   end
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: "users/registrations",
-    passwords: "users/passwords"
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
   devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   root 'homes#index'
   resources :users, only: %i[show]
