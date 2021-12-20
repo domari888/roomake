f1d841c78e043cd86d7ded9cc926a19545d8d195
damari <motokin128@gmail.com>
2021-12-20 17:11:42 +0900

    add: bootstrap の collapse と table を使用してランキング詳細を表示
    collapse を使用した折りたたみで表示することで UI を向上させるため

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,2 +1,9 @@
 module GraphsHelper
+  def result_label(table)
+    table == "likes_average" ? "平均" : "合計"
+  end
+
+  def suffix_name(table)
+    table == "likes_count" || table == "likes_average" ? "いいね !" : "件"
+  end
 end
