0917e328a2cc13173fad6dcf18514de242d3b1c5
damari <motokin128@gmail.com>
2021-12-29 23:35:47 +0900

    clean: rubocop -A を実行してリファクタリング
    ダブルクォーテーションをシングルクォーテーションに修正及び三項演算子の記述方法を修正

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,9 +1,9 @@
 module GraphsHelper
   def result_label(table)
-    table == "likes_average" ? "平均" : "合計"
+    table == 'likes-average' ? '平均' : '合計'
   end
 
   def suffix_name(table)
-    table == "likes_count" || table == "likes_average" ? "いいね !" : "件"
+    ['likes-count', 'likes-average'].include?(table) ? 'いいね !' : '件'
   end
 end
--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,22 +1,21 @@
 Chartkick.options = {
   width: '100%',
-  colors: [ "#baa877",
-            "#a8a9a8",
-            "#947b60",
-            "#E6E7E8",
-            "#E8E9EB",
-            "#EBECED",
-            "#EDEFF0",
-            "#F0F1F2",
-            "#F2F4F5",
-            "#F5F6F7",
-            "#F7F9FA",
-            "#FCFEFF",
-          ],
-  label: "合計",
-  loading: "Loading...",
-  thousands: ",",
-  suffix: "件",
+  colors: ['#baa877',
+           '#a8a9a8',
+           '#947b60',
+           '#E6E7E8',
+           '#E8E9EB',
+           '#EBECED',
+           '#EDEFF0',
+           '#F0F1F2',
+           '#F2F4F5',
+           '#F5F6F7',
+           '#F7F9FA',
+           '#FCFEFF'],
+  label: '合計',
+  loading: 'Loading...',
+  thousands: ',',
+  suffix: '件',
   round: 1,
   legend: false,
   library: {
@@ -43,7 +42,7 @@ Chartkick.options = {
         },
         size: '110%',
         borderWidth: 1,
-        borderColor: "#d3d3d3"
+        borderColor: '#d3d3d3'
       }
     }
   }
