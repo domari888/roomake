2ee7c34af085b051c26e27c787848f701ccc4e7d
damari <motokin128@gmail.com>
2022-01-10 21:56:12 +0900

    add: homes コントローラで最新投稿を 2 件取得してレンダリングを設定
    トップページの Pick Up 部分で最新投稿を 2 件表示してアプリの内容を周知及び最新投稿への UX 向上のため

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,5 @@
 class HomesController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.includes(:user, :photos, :tags, :categories, :likes, :marks).order(created_at: :desc).limit(2)
+  end
 end
