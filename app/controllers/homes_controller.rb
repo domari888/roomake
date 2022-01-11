0213a33465c177a00a75c6b07d648e9e4420fd34
damari <motokin128@gmail.com>
2022-01-11 23:34:51 +0900

    fix: Pick Up のパーシャルファイルを _pick_up.html.erb に修正
    _time_line.html.erb をパーシャルに使用すると未ログイン時に「いいね」または「マーク」ボタンを押すとサーバー側で 401 エラーが発生してしまっていたため Pick Up 用の部分テンプレートを作成して投稿画像・ユーザー名・いいね数・マーク数のみ表示してエラーの原因を排除するため

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def index
-    @posts = Post.includes(:user, :photos, :tags, :categories, :likes, :marks).order(created_at: :desc).limit(2)
+    @posts = Post.includes(:user, :photos).order(created_at: :desc).limit(4)
   end
 end
