050c4b9c7ac8d13ef1698a1d3335e662584cb7e0
damari <motokin128@gmail.com>
2022-01-16 13:29:18 +0900

    add: プロフィール画像登録時のプレビューの処理を変更して削除機能を追加
    画像選択キャンセル時 file_field の値が空になってしまっていたのを解消するため　及び選択されている画像の削除ボタンを設定

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,10 +1,33 @@
-document.addEventListener('turbolinks:load', () => {
-    const previewImage = document.querySelector('.previewImage');
-    previewImage.addEventListener('change', (e) => {
-        const fileReader = new FileReader();
-        fileReader.onload = (function() {
-        document.getElementById('preview').src = fileReader.result;
+$(document).on('turbolinks:load', function(){
+    $(function(){
+        const dataBox = new DataTransfer();
+        const fileField = document.querySelector('input[id=user_avatar]');
+        // 【プロフィール画像プレビュー】
+        $('.avatar-image-field').on('change', function(){
+            if ($(this).val() !== "") {
+                const avatar = $(this).prop('files')[0];
+                const fileReader = new FileReader();
+                if (dataBox.items.length !== 0) {
+                    dataBox.clearData();
+                    $('.avatar-preview, .delete-preview').remove();
+                }
+                dataBox.items.add(avatar)
+                fileField.files = dataBox.files
+                fileReader.onloadend = function(){
+                    const avatarPreview = `<img src="${fileReader.result}" class="rounded-circle avatar-preview bg-light">`;
+                    const deleteButton = '<button type="button" class="btn btn-dark btn-sm rounded-circle delete-preview"><i class="fas fa-times"></i></button>';
+                    $('.avatar-label').prepend(avatarPreview).after(deleteButton);
+                };
+                fileReader.readAsDataURL(avatar);
+            } else {
+                fileField.files = dataBox.files
+            }
+        });
+        // 【プロフィール画像削除】
+        $('.avatar-container').on('click', '.delete-preview', function(){
+            dataBox.clearData();
+            fileField.files = dataBox.files
+            $('.avatar-preview, .delete-preview').remove();
+        });
     });
-    fileReader.readAsDataURL(e.target.files[0]);
-    });
-})
+});
