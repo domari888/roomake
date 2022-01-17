a13188b0a0d0ee26bf2abab6f080c41699f150f5
damari <motokin128@gmail.com>
2022-01-17 23:09:27 +0900

    update: ログイン・新規登録・アカウント情報更新後のリダイレクト先を変更
    Devise のデフォルトでは全てトップページにリダイレクトの設定になっていたためログイン・新規登録後はタイムラインページへ　アカウント情報更新後はユーザーページへリダイレクトするように設定することで UX を向上させるため

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,6 +24,12 @@ module Users
     #   super
     # end
 
+    private
+
+    def after_sign_in_path_for(resource)
+      posts_path
+    end
+
     # protected
 
     # If you have extra params to permit, append them to the sanitizer.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions',
     passwords: 'users/passwords'
   }
   devise_scope :user do
