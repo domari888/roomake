d15fa0e4ab1c617b35bef73cf12b2164f7795986
damari <motokin128@gmail.com>
2022-01-18 15:20:09 +0900

    add: ドラッグ＆ドロップでもプロフィール画像の選択をできるように設定
    画像プレビューの処理を関数化してドラッグ&ドロップまたはファイルから選択の際に関数を呼び出す形でプレビューの処理を共通化

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -3,22 +3,26 @@ $(document).on('turbolinks:load', function(){
         const dataBox = new DataTransfer();
         const fileField = document.querySelector('input[id=user_avatar]');
         // 【プロフィール画像プレビュー】
+        function avatarPreview(avatar){
+            const fileReader = new FileReader();
+            if ($('.avatar-preview').length ) {
+                dataBox.clearData();
+                $('.avatar-preview, .delete-preview').remove();
+            }
+            dataBox.items.add(avatar)
+            fileField.files = dataBox.files
+            fileReader.onloadend = function(){
+                const avatarImage = `<img src="${fileReader.result}" class="rounded-circle avatar-preview bg-light">`;
+                const deleteButton = '<button type="button" class="btn btn-dark btn-sm rounded-circle delete-preview"><i class="fas fa-times"></i></button>';
+                $('.avatar-label').prepend(avatarImage).after(deleteButton);
+            };
+            fileReader.readAsDataURL(avatar);
+        }
+        // 【プロフィール画像選択】
         $('.avatar-image-field').on('change', function(){
             if ($(this).val() !== "") {
                 const avatar = $(this).prop('files')[0];
-                const fileReader = new FileReader();
-                if ($('.avatar-preview').length ) {
-                    dataBox.clearData();
-                    $('.avatar-preview, .delete-preview').remove();
-                }
-                dataBox.items.add(avatar)
-                fileField.files = dataBox.files
-                fileReader.onloadend = function(){
-                    const avatarPreview = `<img src="${fileReader.result}" class="rounded-circle avatar-preview bg-light">`;
-                    const deleteButton = '<button type="button" class="btn btn-dark btn-sm rounded-circle delete-preview"><i class="fas fa-times"></i></button>';
-                    $('.avatar-label').prepend(avatarPreview).after(deleteButton);
-                };
-                fileReader.readAsDataURL(avatar);
+                avatarPreview(avatar);
             } else {
                 fileField.files = dataBox.files
             }
@@ -33,5 +37,23 @@ $(document).on('turbolinks:load', function(){
             }
             $('.avatar-preview, .delete-preview').remove();
         });
+        // 【プロフィール画像ドロップ機能】
+        if (document.querySelector('.avatar-label')) {
+            const avatarDropArea = document.querySelector('.avatar-label');
+            avatarDropArea.addEventListener("dragover", function(e){
+                e.preventDefault();
+                $(this).css({'background-color': '#9e9e9e', 'opacity': '0.5'});
+            },false);
+            avatarDropArea.addEventListener("dragleave", function(e){
+                e.preventDefault();
+                $(this).css('opacity', '1.0').addClass('bg-light');
+            },false);
+            avatarDropArea.addEventListener("drop", function(e) {
+                e.preventDefault();
+                $(this).css('opacity', '1.0').addClass('bg-light');
+                const avatar = e.dataTransfer.files[0];
+                avatarPreview(avatar);
+            });
+        }
     });
 });
