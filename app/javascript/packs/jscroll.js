71b7852628b8680b2488189652f87615dc694e9d
damari <motokin128@gmail.com>
2022-01-29 22:46:29 +0900

    add: 画面サイズが sm 以下の場合マイアイテムの表示を jscroll で表示
    画面サイズが sm 以下の場合は縦長になるので jscroll を設定することで UX の工場につながるため

--- app/javascript/packs/jscroll.js
+++ app/javascript/packs/jscroll.js
@@ -2,29 +2,31 @@ $(document).on('turbolinks:load', function(){
   $(function(){
     const windowSize = $(window).width();
     if (windowSize < 576) {
-      function jscrollOption(tabId){
+      function jscrollOption(jscrollId){
         const Option = {
           loadingHtml: '<div class="d-flex justify-content-center"><div class="spinner-border text-secondary" role="status"><span class="sr-only">Loading...</span></div></div>',
           autoTrigger: true,
           padding: 100,
           nextSelector: 'a[rel=next]',
-          contentSelector: `#${tabId}`,
+          contentSelector: `#${jscrollId}`,
           pagingSelector: '.pagination',
           callback: function(){
-            $(this).children().addClass('show');
+            if (jscrollId !== 'item-list') $(this).children().addClass('show');
             $('.pagination').hide();
           }
         };
-        $(`#${tabId}`).jscroll(Option);
+        $(`#${jscrollId}`).jscroll(Option);
       }
 
+      // マイアイテムの表示に jscroll を使用
+      jscrollOption('item-list');
       // ユーザーページアクセス時にアクティブ状態のタブに jscroll を設定
-        jscrollOption('list-posts');
+      jscrollOption('list-posts');
 
       // タブ切り替え表示の際にアクティブ状態のタブに jscroll を設定
       $('a[data-toggle="list"]').on('shown.bs.tab', function (e) {
-        const tabId = e.target.dataset.tab;
-        jscrollOption(tabId);
+        const jscrollId = e.target.dataset.tab;
+        jscrollOption(jscrollId);
       });
     }
   });
