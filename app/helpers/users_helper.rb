2ada627888aa4f04ec44272286cf5e44dc5e7755
damari <motokin128@gmail.com>
2022-02-01 18:28:29 +0900

    clean: ユーザー詳細ページのリファクタリング
    ページタイトルを独自ヘルパーメソッドを使用して設定 マイアイテムページへのリンクはマイページの場合のみ表示するように設定

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def tab_name(user)
     current_user.id == user.id ? 'marked' : 'item'
   end
+
+  def page_title(user)
+    current_user.id == user.id ? { title: 'My Page', sub_title: 'マイページ' } : { title: 'User Details', sub_title: 'ユーザー詳細' }
+  end
 end
