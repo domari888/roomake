b527ea3ac253123e5bd6c26985635edd52126854
damari <motokin128@gmail.com>
2022-02-04 18:27:05 +0900

    お客様のお名前(カナ)に全角カタカナのバリデーションを設定
    フォーマットを全角カタカナに統一するため

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,7 +1,8 @@
 class Inquiry < ApplicationRecord
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :name, presence: true, length: { maximum: 48 }
-  validates :name_kana, presence: true, length: { maximum: 48 }
+  validates :name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'は全角カタカナで入力してください' }, length: { maximum: 48 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 256 }
   validates :content, presence: true, length: { maximum: 2000 }
   validates :remote_ip, presence: true
