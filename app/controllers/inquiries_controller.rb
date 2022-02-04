a60faca1a0ea59d6ad66af2dd0e81cbbb8838960
damari <motokin128@gmail.com>
2022-02-04 18:34:26 +0900

    add: お問い合わせフォームを作成
    フロント側でのバリデーションを実装 バリデーションエラーの際はレンダリングさせ入力値を保持させるように設定

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,7 +1,22 @@
 class InquiriesController < ApplicationController
   def index
+    @inquiry = Inquiry.new
   end
 
   def create
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.save
+      InquiryMailer.user_email(@inquiry).deliver_now
+      InquiryMailer.admin_email(@inquiry).deliver_now
+      redirect_to posts_path, notice: 'お問い合わせが完了しました'
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :name_kana, :email, :content).merge(remote_ip: request.remote_ip)
   end
 end
