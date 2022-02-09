853ad8fe212bbc6d7c244f2ecb9b03ca2f9eb972
damari <motokin128@gmail.com>
2022-02-09 22:31:04 +0900

    add: お問い合わせフォームのリンクをページ下部に作成
    画面サイズが sm 以下の場合はトップページにのみリンクを表示してそれ以外のページではナビバー内に表示 md 以上の場合は常にページ下部に表示

--- app/helpers/inquiries_helper.rb
+++ app/helpers/inquiries_helper.rb
@@ -1,2 +1,9 @@
 module InquiriesHelper
+  def footer_display
+    if controller_name == 'homes'
+      'footer-before-signing' unless user_signed_in?
+    else
+      'd-sm-block d-none'
+    end
+  end
 end
