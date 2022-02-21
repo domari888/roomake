af3cceca9476d4e4c605bdafbd85bba02df598c5
damari <motokin128@gmail.com>
2022-02-22 03:26:01 +0900

    add: Comment モデルの Form を設定
    投稿に紐づく形でルーティングを設定しているため投稿選択のフォームを非表示に設定

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Comment do
+  permit_params :content, :user_id
   belongs_to :post
   includes :user
 
@@ -10,6 +11,15 @@ ActiveAdmin.register Comment do
     column :created_at
     column :updated_at
     actions
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :user, as: :select, collection: User.order(name: :asc), include_blank: '選択してください'
+      f.input :content, hint: '140 文字以下', input_html: { maxlength: 140 }
+    end
+    f.actions
   end
 
   remove_filter :post
