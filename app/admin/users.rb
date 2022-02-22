6f3a5e3987f422caff274fe8ec390c4a8e6ec40e
damari <motokin128@gmail.com>
2022-02-22 22:07:56 +0900

    add: User モデルの Form を部分テンプレート化
    プロフィール画像の上書きしかできなかったが部分テンプレートではプロフィール画像の削除ボタンを新たに設定

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :name, :password, :password_confirmation, :email, :age, :address, :profile, :household, :favorite_items, :avatar, :avatar_cache, :terms_of_use
+  permit_params :name, :password, :password_confirmation, :email, :age, :address, :profile, :household, :favorite_items, :avatar, :avatar_cache, :terms_of_use, :remove_avatar
 
   index do
     selectable_column
@@ -33,22 +33,5 @@ ActiveAdmin.register User do
   filter :address, as: :select
   filter :household, as: :select
 
-  form do |f|
-    f.semantic_errors
-    f.inputs do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-      f.input :name
-      f.input :age, as: :select, collection: User.ages_i18n.invert
-      f.input :address, as: :select, collection: User.addresses_i18n.invert
-      f.input :household, as: :select, collection: User.households_i18n.invert
-      f.input :favorite_items
-      f.input :profile
-      f.input :avatar
-      f.input :avatar_cache, as: :hidden
-      f.input :terms_of_use, as: :hidden, input_html: { value: '1' }
-    end
-    f.actions
-  end
+  form partial: 'form'
 end
