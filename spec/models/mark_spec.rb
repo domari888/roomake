7a66ee876f2606c19c083e29e5a05a8e0773b41c
damari <motokin128@gmail.com>
2022-03-28 11:37:59 +0900

    add: Mark モデルのモデルスペックを作成
    user_id と post_id のバリデーションとユニーク制約のテストを作成

--- spec/models/mark_spec.rb
+++ spec/models/mark_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Mark, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    subject { mark.valid? }
+
+    context 'データが条件を満たすとき' do
+      let(:mark) { build(:mark) }
+      it '保存できること' do
+        expect(subject).to eq true
+      end
+    end
+
+    context 'user_id が空のとき' do
+      let(:mark) { build(:mark, user_id: nil) }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(mark.errors.messages[:user]).to include 'を入力してください'
+      end
+    end
+
+    context 'post_id が空のとき' do
+      let(:mark) { build(:mark, post_id: nil) }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(mark.errors.messages[:post]).to include 'を入力してください'
+      end
+    end
+
+    context '同じ投稿をマークした場合' do
+      let(:user) { build(:user) }
+      let(:post) { build(:post) }
+      let(:mark) { build(:mark, user: user, post: post) }
+      before do
+        create(:mark, user: user, post: post)
+      end
+
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(mark.errors.messages[:user_id]).to include 'は同じ投稿に2回以上マークはできません'
+      end
+    end
+  end
 end
