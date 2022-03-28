540f5af42337e048f27d7a4f54d3b8fe9193dec0
damari <motokin128@gmail.com>
2022-03-28 10:39:39 +0900

    add: Photo モデルのモデルスペックを作成
    image カラムのバリデーションテストを作成

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    subject { photo.valid? }
+
+    context 'データが条件を満たすとき' do
+      let(:photo) { build(:photo) }
+      it '保存できること' do
+        expect(subject).to eq true
+      end
+    end
+
+    context 'image の画像サイズが 5MB 以上のとき' do
+      let(:photo) { build(:photo, image: Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/rspec_size_test.jpg'))) }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(photo.errors.messages[:image]).to include 'ファイルを5MBバイト以下のサイズにしてください'
+      end
+    end
+
+    context 'image の拡張子が .jpeg .jpg .png 以外のとき' do
+      let(:photo) { build(:photo, image: Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/rspec_extension_test.tiff'))) }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(photo.errors.messages[:image]).to include '"tiff"ファイルのアップロードは許可されていません。アップロードできるファイルタイプ: jpg, jpeg, png'
+      end
+    end
+  end
 end
