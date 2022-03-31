121b28a7adb871484211dd03288736773bea1cba
damari <motokin128@gmail.com>
2022-03-31 22:33:40 +0900

    add: Tag モデルのバリデーションテストを作成
    条件を満たすとき保存できること name が空のときエラーが発生すること

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    subject { tag.valid? }
+
+    context 'データが条件を満たすとき' do
+      let(:tag) { build(:tag) }
+      it '保存されること' do
+        expect(subject).to eq true
+      end
+    end
+
+    context 'name が空のとき' do
+      let(:tag) { build(:tag, name: '') }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(tag.errors.messages[:name]).to include 'を入力してください'
+      end
+    end
+  end
 end
