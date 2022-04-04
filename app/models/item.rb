231a42a789de68f290779873e7d6802f1b25e44a
damari <motokin128@gmail.com>
2022-04-04 18:01:01 +0900

    update: Item モデルのバリデーションをカスタムメソッドを使用して追加
    同じアイテムを追加したときのバリデーションをフロント側でしか実装していなかったのでモデル側でも定義することでバリデーションを強化するため

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
+  validate :unique_item?, if: -> { user_id.present? }
+
+  def unique_item?
+    user = User.find(user_id)
+    return unless user.items.find_by(name: name)
+
+    errors.add(:name, 'は既に登録されています')
+  end
 end
