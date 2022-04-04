1e5836724213d0895a9ad63a6baf1f6aed585945
damari <motokin128@gmail.com>
2022-04-04 23:01:09 +0900

    add: KnowHow モデルの genre を enum で設定
    レコードからの取り出しを容易にするため

--- app/models/know_how.rb
+++ app/models/know_how.rb
@@ -2,4 +2,6 @@ class KnowHow < ApplicationRecord
   validates :genre, presence: true
   validates :title, presence: true
   validates :content, presence: true
+
+  enum genre: { all_rooms: 1, kitchen: 2, toilet: 3, bath: 4, washbasin: 5, balcony: 6, entrance: 7 }
 end
