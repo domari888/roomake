e3731c031937b574f6351d94024dcad70cd46aea
damari <motokin128@gmail.com>
2022-04-05 14:39:18 +0900

    add: ノウハウページのルーティングとリンクを設定
    only を使用して index show のみルーティングを設定 トップページとナビバーにノウハウページへのパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   resources :graphs, only: %i[index]
   resources :search_items, only: %i[index]
   resources :inquiries, only: %i[index create]
+  resources :know_hows, only: %i[index show]
 end
