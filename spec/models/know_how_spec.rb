b7c966e04498b8e736a32c224aaced38b141e29b
damari <motokin128@gmail.com>
2022-04-08 16:54:58 +0900

    add: KnowHow モデルのバリデーションテストを作成
    データが条件を満たすとき保存できること genre title content のいづれか空のときエラーが発生すること

--- spec/models/know_how_spec.rb
+++ spec/models/know_how_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe KnowHow, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    subject { know_how.valid? }
+
+    context 'データが条件を満たすとき' do
+      let(:know_how) { build(:know_how) }
+      it '保存できること' do
+        expect(subject).to eq true
+      end
+    end
+
+    context 'genre が空のとき' do
+      let(:know_how) { build(:know_how, genre: '') }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(know_how.errors.messages[:genre]).to include 'を入力してください'
+      end
+    end
+
+    context 'title が空のとき' do
+      let(:know_how) { build(:know_how, title: '') }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(know_how.errors.messages[:title]).to include 'を入力してください'
+      end
+    end
+
+    context 'content が空のとき' do
+      let(:know_how) { build(:know_how, content: '') }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(know_how.errors.messages[:content]).to include 'を入力してください'
+      end
+    end
+  end
 end
