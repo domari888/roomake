0d9e3d4a5599c2d32454f1145e9273c02d0ffb82
damari <motokin128@gmail.com>
2022-04-15 15:06:23 +0900

    add: Progress モデルのバリデーションテストを作成
    データが条件を満たすとき保存できること user_id know_how_id が空のとき ユニークでないときエラーが発生する

--- spec/models/progress_spec.rb
+++ spec/models/progress_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Progress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    subject { progress.valid? }
+
+    context 'データが条件を満たすとき' do
+      let(:progress) { build(:progress) }
+      it '保存できること' do
+        expect(subject).to eq true
+      end
+    end
+
+    context 'user_id が空のとき' do
+      let(:progress) { build(:progress, user_id: '') }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(progress.errors.messages[:user]).to include 'を入力してください'
+      end
+    end
+
+    context 'know_how_id が空のとき' do
+      let(:progress) { build(:progress, know_how_id: '') }
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(progress.errors.messages[:know_how]).to include 'を入力してください'
+      end
+    end
+
+    context '同じノウハウをチェックしたとき' do
+      let(:user) { build(:user) }
+      let(:know_how) { build(:know_how) }
+      let(:progress) { build(:progress, user: user, know_how: know_how) }
+      before do
+        create(:progress, user: user, know_how: know_how)
+      end
+
+      it 'エラーが発生する' do
+        expect(subject).to eq false
+        expect(progress.errors.messages[:user_id]).to include 'は同じノウハウに2回以上チェックはできません'
+      end
+    end
+  end
 end
