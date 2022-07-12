a6db2e6f7eff7c13218d64518e0070c90db7693b
damari <motokin128@gmail.com>
2022-07-12 23:12:10 +0900

    update: トップページでのコンテナ幅を全てのブレークポイントで100%になるように修正
    背景色と背景画像を画面幅で表示するためにトップページではcontainer-fluidを設定してFeature・PickUp・Functionなどのコンテンツはcssで最大幅を設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def max_width
     if devise_controller? || controller_name == 'inquiries'
       'mw-sm'
+    elsif controller_name == 'homes'
+      'px-0'
     else
       'mw-xl'
     end
