72efa42ce7eef829eb8a803c082bcc4e55fa0beb
damari <motokin128@gmail.com>
2022-08-12 22:57:48 +0900

    add: ActionMailer のテストを作成
    期待した通りのメールが送受信されることを検証するため

--- spec/mailers/inquiry_mailer_spec.rb
+++ spec/mailers/inquiry_mailer_spec.rb
@@ -1,31 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe InquiryMailer, type: :mailer do
-  describe 'user_email' do
-    let(:mail) { InquiryMailer.user_email }
+RSpec.describe 'お問い合わせ機能', type: :mailer do
+  describe '#user_email' do
+    let(:inquiry) { build(:inquiry) }
+    let(:mail) { InquiryMailer.user_email(inquiry).deliver_now }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('User email')
-      expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['from@example.com'])
-    end
-
-    it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.subject).to eq('【Roomake】お問い合わせを受付いたしました')
+      expect(mail.to).to eq([inquiry.email])
+      expect(mail.from).to eq(['roomake.contact@gmail.com'])
     end
   end
 
-  describe 'admin_email' do
-    let(:mail) { InquiryMailer.admin_email }
+  describe '#admin_email' do
+    let(:inquiry) { build(:inquiry) }
+    let(:mail) { InquiryMailer.admin_email(inquiry).deliver_now }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Admin email')
-      expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['from@example.com'])
-    end
-
-    it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.subject).to eq('【Roomake】お客様よりお問い合わせがありました')
+      expect(mail.to).to eq(['roomake.contact@gmail.com'])
+      expect(mail.from).to eq(['roomake.contact@gmail.com'])
     end
   end
 end
