e0a51390ebe8fd0ddbc6f1ef68cb64743b9a9c73
damari <motokin128@gmail.com>
2022-08-20 00:18:47 +0900

    add: アイテム検索フォームを空文字で送信されてしまった際のフラッシュメッセージを追加
    JS を解除されて送信された際にエラー画面が表示されるのを防ぐため

--- app/controllers/search_items_controller.rb
+++ app/controllers/search_items_controller.rb
@@ -1,7 +1,6 @@
 class SearchItemsController < ApplicationController
   def index
-    return unless params[:keyword]
-
+    flash.now[:alert] = 'キーワードを入力して下さい' if params[:keyword].blank?
     @search_items = RakutenWebService::Ichiba::Product.search(keyword: params[:keyword])
   end
 end
