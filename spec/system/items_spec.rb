857acdaa910b78ef6d6061f2bf5d3576b0935a8c
damari <motokin128@gmail.com>
2022-08-20 14:40:23 +0900

    update: アイテム追加機能のシステムスペックを修正
    link_to do endブロック間や登録状況によってボタンの表示を修正したためそれに対応したテストコードに修正

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe 'アイテム機能', type: :system do
   end
 
   describe 'アイテム追加機能' do
+    let(:user) { create(:user) }
     context '検索したアイテムの追加ボタンを押下したとき' do
-      let(:user) { create(:user) }
       let(:item) { build(:item, user: user) }
       it 'そのアイテムがマイアイテムに追加されること' do
         visit user_items_path(user)
@@ -23,23 +23,52 @@ RSpec.describe 'アイテム機能', type: :system do
         expect do
           within '.search-item-group' do
             click_on 'アイテムを追加する'
-            fill_in 'keyword', with: 'テストアイテム'
+            fill_in 'keyword', with: item.name
             find('.input-group-append button').click
           end
           within '#search-item-list' do
-            all('li')[0].click_on '追加'
+            expect(first('li')).to have_selector "img[src='#{item.remote_image_url}']"
+            expect(first('li')).to have_content item.name
+            expect(first('li')).to have_content item.genre
+            first('li').click_on '追加'
           end
-          expect(page).to have_content "#{user.items.first.name} をマイアイテムに追加しました"
-          expect(page).to have_selector "img[src='#{user.items.first.image.url}']"
-          expect(page).to have_content user.items.first.name
-          expect(page).to have_content user.items.first.genre
+          expect(page).to have_content "#{item.name} をマイアイテムに追加しました"
+          within "#my-item-#{user.items.last.id}" do
+            expect(page).to have_selector "img[src='#{user.items.last.image.url}']"
+            expect(page).to have_content item.name
+            expect(page).to have_content item.genre
+          end
         end.to change { user.items.count }.by(1)
       end
     end
 
-    context '追加ボタンを押下したアイテムが既に登録済みのとき' do
-      let(:user) { create(:user) }
+    context 'アイテムが既に登録済みのとき' do
       let(:item) { create(:item, image: nil, user: user) }
+      it 'ボタンが【登録済み】と表示されること' do
+        visit user_items_path(user)
+        product = [{ 'affiliateUrl' => Faker::Internet.url, 'mediumImageUrl' => item.remote_image_url, 'productName' => item.name, 'genreName' => item.genre }]
+        search_items_mock = class_double(RakutenWebService::Ichiba::Product)
+        allow(search_items_mock).to receive(:search)
+        allow(RakutenWebService::Ichiba::Product).to receive(:search).and_return(product)
+        expect { search_items_mock.search }.not_to raise_error
+        expect do
+          within '.search-item-group' do
+            click_on 'アイテムを追加する'
+            fill_in 'keyword', with: item.name
+            find('.input-group-append button').click
+          end
+          within '#search-item-list' do
+            expect(first('li')).to have_selector "img[src='#{item.remote_image_url}']"
+            expect(first('li')).to have_content item.name
+            expect(first('li')).to have_content item.genre
+            expect(first('li')).to have_button '登録済み', disabled: true
+          end
+        end.to change { user.items.count }.by(0)
+      end
+    end
+
+    context 'アイテム追加の条件を満たさないとき' do
+      let(:item) { build(:item, name: nil, user: user) }
       it 'エラーが発生すること' do
         visit user_items_path(user)
         product = [{ 'affiliateUrl' => Faker::Internet.url, 'mediumImageUrl' => item.remote_image_url, 'productName' => item.name, 'genreName' => item.genre }]
@@ -54,34 +83,7 @@ RSpec.describe 'アイテム機能', type: :system do
             find('.input-group-append button').click
           end
           within '#search-item-list' do
-            all('li')[0].click_on '追加'
-          end
-          expect(page).to have_content "#{user.items.first.name} は既に登録されています"
-          expect(page).to have_selector "img[src='#{user.items.first.image.url}']"
-          expect(page).to have_content user.items.first.name
-          expect(page).to have_content user.items.first.genre
-        end.to change { user.items.count }.by(0)
-      end
-    end
-
-    context 'アイテム追加の条件を満たさないとき' do
-      let(:user) { create(:user) }
-      let(:item) { build(:item, name: nil, user: user) }
-      it 'エラーが発生すること' do
-        visit user_items_path(user)
-        product = [{ 'affiliateUrl' => Faker::Internet.url, 'mediumImageUrl' => item.remote_image_url, 'productName' => '', 'genreName' => item.genre }]
-        search_items_mock = class_double(RakutenWebService::Ichiba::Product)
-        allow(search_items_mock).to receive(:search)
-        allow(RakutenWebService::Ichiba::Product).to receive(:search).and_return(product)
-        expect { search_items_mock.search }.not_to raise_error
-        expect do
-          within '.search-item-group' do
-            click_on 'アイテムを追加する'
-            fill_in 'keyword', with: 'テストアイテム'
-            find('.input-group-append button').click
-          end
-          within '#search-item-list' do
-            all('li')[0].click_on '追加'
+            first('li').click_on '追加'
           end
           expect(page).to have_content 'アイテムを追加することができませんでした'
         end.to change { user.items.count }.by(0)
