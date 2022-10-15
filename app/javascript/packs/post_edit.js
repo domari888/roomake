7ac08cc304c8ab127eeeeb15b3a56ce4d7c3e32f
damari <motokin128@gmail.com>
2022-10-15 16:46:24 +0900

    update: 投稿編集モーダルを開く直前にフォームの内容を保存されている内容に全て置き換えるように修正
    投稿編集モーダルを開く直前に保存されている内容と前回フォームで入力した内容の配列を作成して比較し差異がある場合内容を置き換えるようにしていたがデータ量も少なく可読性を重視するため

--- app/javascript/packs/post_edit.js
+++ app/javascript/packs/post_edit.js
@@ -8,64 +8,33 @@ $(document).on('turbolinks:load', function() {
       // 投稿内容を表示
       $('#edit_post_form_content').val(content)
       // 投稿画像を表示
-      const previewData = $('#edit-drop').prevAll('.edit-preview-item');
-      const targetIds = $.map(previewData, function(preview) {
-        return $(preview).data('id');
-      });
-      const photoIds = $.map(photos, function(photo) {
-        return photo.id;
-      });
-      if (targetIds.toString() !== photoIds.toString()) {
-        $('#edit-drop').prevAll().remove();
-        photos.forEach(function(photo){
-        const html = `<div class="edit-preview-item" data-id="${photo.id}">
-                        <img src="${photo.image.url}" class="preview-image">
-                        <button type="button" class="btn btn-dark-gray btn-sm rounded-circle delete-preview" data-action="edit"><i class="fas fa-times"></i></button>
-                      </div>`;
-          $('#edit-drop').before($(html));
-          const previewItemLength = $('#edit-drop').prevAll('.edit-preview-item').length;
-          if ( previewItemLength >= 1 && previewItemLength <= 6 ) {
-            $('#edit-button').prop('disabled', false);
-            if ( previewItemLength === 6 ) {
-              $('#edit-drop').hide();
-            }
+      $('#edit-drop').prevAll().remove();
+      photos.forEach(function(photo){
+      const html = `<div class="edit-preview-item" data-id="${photo.id}">
+                      <img src="${photo.image.url}" class="preview-image">
+                      <button type="button" class="btn btn-dark-gray btn-sm rounded-circle delete-preview" data-action="edit"><i class="fas fa-times"></i></button>
+                    </div>`;
+        $('#edit-drop').before($(html));
+        const previewItemLength = $('#edit-drop').prevAll('.edit-preview-item').length;
+        if ( previewItemLength >= 1 && previewItemLength <= 6 ) {
+          $('#edit-button').prop('disabled', false);
+          if ( previewItemLength === 6 ) {
+            $('#edit-drop').hide();
           }
-        });
-      }
+        }
+      });
 
+      $('.checkbox-visibility').prop('checked', false);
       // 投稿タグの表示
-      const checkedTag = $('.tags-check-box:checked');
-      const checkedTagIds = $.map(checkedTag, function(tag){
-        const previewTag = $(tag).val();
-        return parseInt(previewTag);
+      tags.forEach(function(tag){
+        const targetTag = $(`#edit_post_form_tag_ids_${tag.id}`);
+        $(targetTag).prop('checked', true);
       });
-      const tagIds = $.map(tags, function(tag){
-        return tag.id;
-      });
-      if(checkedTagIds.toString() !== tagIds.toString()){
-        $('.tags-check-box').prop('checked', false);
-        tags.forEach(function(tag){
-          const targetTag = $(`#edit_post_form_tag_ids_${tag.id}`);
-          $(targetTag).prop('checked', true);
-        });
-      }      
-
       // 投稿カテゴリーの表示
-      const checkedCategory = $('.categories-check-box:checked');
-      const checkedCategoryIds = $.map(checkedCategory, function(category){
-        const previewCategory = $(category).val();
-        return parseInt(previewCategory);
+      categories.forEach(function(category){
+        const targetCategory = $(`#edit_post_form_category_ids_${category.id}`);
+        $(targetCategory).prop('checked', true);
       });
-      const categoryIds = $.map(categories, function(category){
-        return category.id;
-      });
-      if(checkedCategoryIds.toString() !== categoryIds.toString()){
-        $('.categories-check-box').prop('checked', false);
-        categories.forEach(function(category){
-          const targetCategory = $(`#edit_post_form_category_ids_${category.id}`);
-          $(targetCategory).prop('checked', true);
-        });
-      }
     });
   });
 });
