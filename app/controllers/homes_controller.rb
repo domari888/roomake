6857b2e9074955526a5d3a8036506d10f46b69f1
damari <motokin128@gmail.com>
2022-11-10 17:04:36 +0900

    add: トップページに表示している機能一覧を部分テンプレート化
    機能一覧の表示が冗長だったため部分テンプレート化して簡素化 リンク先をヘルパーメソッドを使用して定義 機能一覧のビジュアル画像のサイズを画面サイズ毎に変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def index
+    @homes = Home.order(id: :asc)
     @posts = Post.includes(:user, :photos).order(created_at: :desc).limit(4)
   end
 end
--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,9 +1,14 @@
 module HomesHelper
-  def user_item_page
-    if user_signed_in?
-      user_items_path(current_user)
-    else
-      search_items_path
+  def function_page(title)
+    case title
+    when 'Time Line'
+      posts_path
+    when 'My Item'
+      user_signed_in? ? user_items_path(current_user) : search_items_path
+    when 'Know How'
+      know_hows_path
+    when 'Data'
+      graphs_path
     end
   end
 end
